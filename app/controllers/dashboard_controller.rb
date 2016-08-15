ec470712d6f4561d9cb56ab5f2b3366d2d70bb8d
wedgey <desmondpoon5@hotmail.com>
2016-08-15 23:13:34 +0000

    Implemented timeline that displays tasks the farmer is required to do

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,20 +4,24 @@ class DashboardController < ApplicationController
     @user = User.find(current_user)
     @farm = @user.farms.exists?(params[:id]) ? @user.farms.find(params[:id]) : @user.farms.first
     @activities = Activity.all
-    @water_activity = {}
-    @farm.last_watered.each do |ele|
-      @water_activity[ele.sub_farm_id] = ele.date
-    end
+    # @water_activity = {}
+    # @farm.last_watered.each do |ele|
+    #   @water_activity[ele.sub_farm_id] = ele.date
+    # end
 
-    @fertilizer_activity = {}
-    @farm.last_fertilized.each do |ele|
-      @fertilizer_activity[ele.sub_farm_id] = ele.date
-    end
+    # @fertilizer_activity = {}
+    # @farm.last_fertilized.each do |ele|
+    #   @fertilizer_activity[ele.sub_farm_id] = ele.date
+    # end
 
-    @chemical_activity = {}
-    @farm.last_chemicalled.each do |ele|
-      @chemical_activity[ele.sub_farm_id] = ele.date
-    end
+    # @chemical_activity = {}
+    # @farm.last_chemicalled.each do |ele|
+    #   @chemical_activity[ele.sub_farm_id] = ele.date
+    # end
+
+    @water_activity = @farm.last_watered.first
+    @chemical_activity = @farm.last_chemicalled.first
+    @fertilizer_activity = @farm.last_fertilized.first
 
     @water_activity = @water_activity.to_json
     @chemical_activity = @chemical_activity.to_json
